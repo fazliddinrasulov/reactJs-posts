3f6eb7cc0091bdb07b0884fe85739e1c493c0657
Fazliddin <fazliddin0815@gmail.com>
2023-09-01 20:22:52 +0500

    bug is fixed

--- src/pages/Unsplash/Post.jsx
+++ src/pages/Unsplash/Post.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Post.css";
-import { useNavigate } from "react-router-dom";
 
 const Post = ({ urls, user }) => {
-  const [showBtn, setShowBtn] = useState(false);
-  const navigate = useNavigate();
   return (
     <article className="single-post">
-      <img src={urls.regular} alt="" className="img" />
-      <p className="post-author">{user.name}</p>
+      <img src={urls?.regular} alt="" className="img" />
+      <p className="post-author">{user?.name}</p>
     </article>
   );
 };
